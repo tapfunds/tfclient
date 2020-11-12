39d8ed99bdc283655db9808184fbe706b8ceb449
Qwelian Tanner <qweliantanner@gmail.com@users.noreply.github.com>
2020-11-12 03:17:18 -0500

    format staged

--- src/Components/Plaid/Balance.js
+++ src/Components/Plaid/Balance.js
@@ -1,4 +1,4 @@
-import React, { useContext, useCallback, useEffect, useState  } from "react";
+import React, { useContext, useCallback, useEffect, useState } from "react";
 import { TokenContext } from "../../utils/TokenProvider";
 import axios from "axios";
 import qs from "qs";
@@ -6,7 +6,7 @@ import qs from "qs";
 const balenceURL = `http://localhost:55375/api/balance`;
 function Balance({ props }) {
   const tokes = useContext(TokenContext);
-  const [balances, setBalences] = useState([])
+  const [balances, setBalences] = useState([]);
 
   const fetchBalance = useCallback(() => {
     const config = {
@@ -20,35 +20,33 @@ function Balance({ props }) {
     for (let i = 0; i < tokes.length; i++) {
       config.data = qs.stringify({ access_token: tokes[i].accesstoken });
       promises.push(
-        axios(config)
-        .then(response =>{ 
+        axios(config).then((response) => {
           users.push(response.data.accounts);
         })
-
-      )
-
+      );
     }
     Promise.all(promises).then(() => setBalences(users));
-
   }, [tokes]);
 
+
+  
   useEffect(() => {
     fetchBalance();
   }, [fetchBalance]);
 
-  let n = []
-  let c = []
-  let a = []
-  let l = []
-  
+  let n = [];
+  let c = [];
+  let a = [];
+  let l = [];
+
   for (let i = 0; i < balances.length; i++) {
-    balances[i].map(item => (n.push(item.name)))
-    balances[i].map(item => (c.push(item.balances.current)))
-    balances[i].map(item => (a.push(item.balances.available)))
-    balances[i].map(item => (l.push(item.balances.limit)))
+    balances[i].map((item) => n.push(item.name));
+    balances[i].map((item) => c.push(item.balances.current));
+    balances[i].map((item) => a.push(item.balances.available));
+    balances[i].map((item) => l.push(item.balances.limit));
   }
 
-  let arr = []
+  let arr = [];
 
   for (let i = 0; i < n.length; i++) {
     arr.push({
@@ -57,15 +55,14 @@ function Balance({ props }) {
       current: c[i],
       available: a[i],
       limit: l[i],
-  });
+    });
   }
-
 
   return (
     <div>
-      {arr.map(item => (
+      {arr.map((item) => (
         <li key={item.item_num}>
-          {item.name} | {item.current} | {item.available} | {item.limit}
+          Account Name: {item.name} | Current Balance: {item.current} | Available Balance: {item.available} | Account Limit: {item.limit}
         </li>
       ))}
     </div>
