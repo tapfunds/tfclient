4c0ccb32b401b40fea590aca0df0116ad2b257d6
Qwelian D. Tanner <qweliantanner@gmail.com>
2020-12-17 22:45:05 -0500

    404 page update

--- src/Pages/FourOhFour.js
+++ src/Pages/FourOhFour.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React from "react";
 import image from "../billDuke.jpg";
+import { StyleSheet, css } from "aphrodite";
 
+const styles = StyleSheet.create({
+  imgwrapper: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textwrapper: {
+    display: "flex",
+    flexDirection: "column",
+    textAlign: "center",
+  },
+});
 
-function FourOhFour(){
-    return(
-        <div>
+function FourOhFour() {
+  return (
+    <div>
+      <h2 className={css(styles.textwrapper)}>You know you done messed up right...you know that dont you</h2>
+        <div className={css(styles.imgwrapper)} >
+        <img src={image} alt="Bill Duke told you" />
 
-            <h2>You know you done messed up right...you know that dont you</h2>
-            <div>
-                <img src={image} alt="Bill Duke told you" />
-            </div>
-      </div>
-    )
+        </div>
+    </div>
+  );
 }
 
 export default FourOhFour;
