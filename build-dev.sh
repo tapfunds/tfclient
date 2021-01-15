5133710ca465b7583b491f3cf5790ee2ae03fa1e
Qwelian D. Tanner <qweliantanner@gmail.com>
2021-01-15 18:31:47 -0500

    kube deploy fine tune

--- build-dev.sh
+++ build-dev.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-yarn run build
+
 docker-compose -f docker-compose.dev.yml up -d --build
+docker tag tfclient_client qweliant/tfclient:latest
+docker push qweliant/tfclient
+kubectl delete service,deployment tfclient
 kubectl apply -f kubes.yaml
