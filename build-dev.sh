d2be969efeb24c75709e3e38435f5a9636646ec7
Qwelian D. Tanner <qweliantanner@gmail.com>
2021-01-16 12:18:12 -0500

    moved orchestration to seperate repo

--- build-dev.sh
+++ build-dev.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
 docker-compose -f docker-compose.dev.yml up -d --build
 docker tag tfclient_client qweliant/tfclient:latest
 docker push qweliant/tfclient
-kubectl delete service,deployment tfclient
-kubectl apply -f kubes.yaml
